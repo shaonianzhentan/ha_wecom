7dc9b096ac106c4be37aebd3f975e98334de371b
shaonianzhentan <ilovevar@gmail.com>
2024-09-19 10:09:22 +0800

    修复代码错误

--- custom_components/ha_wecom/assist.py
+++ custom_components/ha_wecom/assist.py
@@ -9,16 +9,16 @@ async def async_assistant(hass, text):
 
     for pipeline in pipelines:
         if pipeline.id == preferred_pipeline:
-        conversation_result = await async_converse(
-            hass=hass,
-            text=text,
-            context=Context(),
-            conversation_id=None,
-            device_id=None,
-            language=hass.config.language,
-            agent_id=pipeline.conversation_engine,
-        )
-        intent_response = conversation_result.response
-        speech = intent_response.speech.get('plain')
-        if speech is not None:
-            return speech.get('speech')
+            conversation_result = await async_converse(
+                hass=hass,
+                text=text,
+                context=Context(),
+                conversation_id=None,
+                device_id=None,
+                language=hass.config.language,
+                agent_id=pipeline.conversation_engine,
+            )
+            intent_response = conversation_result.response
+            speech = intent_response.speech.get('plain')
+            if speech is not None:
+                return speech.get('speech')
